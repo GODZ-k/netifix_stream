3c20ee30bcdb9a5c8654532a121708eed514f72c
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-11-26 17:44:44 +0530

    related tags

--- detail/views.py
+++ detail/views.py
@@ -5,8 +5,10 @@ from .models import *
 def detail(request,slug):
     items = get_object_or_404(movie, slug=slug)
     related_categories=items.category.all().distinct()
+    related_tags=movie.objects.filter(tags=items.tags).exclude(slug=slug).distinct()[:20]
     data={
         "items":items,
         "related_categories": related_categories,
+        "related_tags":related_tags
         }
     return render(request,"detail.html",data)

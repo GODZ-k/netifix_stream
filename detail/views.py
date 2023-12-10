2258f9c13b25ab0bc77c8270f250204fe7083f44
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-12-10 12:40:56 +0530

    v1.0.6

--- detail/views.py
+++ detail/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render,get_object_or_404
 from .models import *
+from detail.models import *
 from downloads.models import *
 # Create your views here.
 def detail(request,slug):
     items = get_object_or_404(movie, slug=slug)
     related_categories=items.category.all().distinct()
     related_tags=movie.objects.filter(tags=items.tags).exclude(slug=slug).distinct()[:20]
-    download_detail=download()
+    download_detail=download(items)
+
     data={
         "items":items,
         "related_categories": related_categories,
@@ -16,8 +18,8 @@ def detail(request,slug):
     return render(request,"detail.html",data)
 
 
-def download():
-    download_movie=downloads.objects.all()
+def download(items):
+    download_movie=downloads.objects.filter(movie_name=items)
     return{
         "download":download_movie,
     }

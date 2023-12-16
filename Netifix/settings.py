db5b4b9ed306b96161bf1708544ea98369459166
Tanmay khatri <131422684+GODZ-k@users.noreply.github.com>
2023-12-16 13:19:47 +0530

    add

--- Netifix/settings.py
+++ Netifix/settings.py
@@ -67,7 +67,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
